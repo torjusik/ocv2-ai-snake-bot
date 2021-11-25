6bf89e488f4c2f85e72bf4cd57897a3b7725c103
Torjus Isaksen_Kleiv <torjuskleiv@gmail.com>
2021-11-25 21:56:15 +0100

    yes

---
+++ main.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+import pyautogui
+from pygetwindow import windowcapture
+class Main():
+    def __init__(self, ):
+        
+        
+        super(Main, self).__init__()
+        
+    
+
+if __name__ == '__main__':
+    main = Main()
+    main.run()
+
